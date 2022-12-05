5d87aa1ce36986d7e50abba71fe86ad63047e537
Arseny-Korobeinikov <arsenikorob@gmail.com>
2022-12-06 00:00:05 +0300

    File modification

--- KorobeinikovAP/Practice5/Practice5/Source.c
+++ KorobeinikovAP/Practice5/Practice5/Source.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <windows.h>
+#include <tchar.h>
 
-
+int f_input_data();
 
 int main() {
-
+	
+	met = f_input_data();
+	f
+	
 }
+
+
+
+int f_input_data() {
+	printf("Choose a sorting method:\n1 - Bubble\n2 - Inserts\n3 - Merge");
+}
+
+
