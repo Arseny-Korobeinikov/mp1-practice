d0d450613a6999a32b394e98571200e60752233a
Arseny-Korobeinikov <arsenikorob@gmail.com>
2022-12-06 23:28:08 +0300

    File modification

--- KorobeinikovAP/Practice5/Practice5/Source.c
+++ KorobeinikovAP/Practice5/Practice5/Source.c
@@ -1,3 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,14 +8,15 @@
 #include <string.h>
 #include <corecrt_wstring.h>
 
-#define ull unsigned long long
-#define N 10
+typedef unsigned long long ull;
+typedef long long ll;
+#define N 100
 
 void insert_sort(int a[], int n[]);
 void bubble_sort(int a[], int n[]);
 void merge(int a[], int l, int r, int m);
 void merge_sort(int a[], int n);
-
+void res(WIN32_FIND_DATA names[], ull* size, int *ind, int n, char hw);
 
 
 
@@ -30,10 +33,16 @@ int main() {
 		ind[i] = i;
 	}
 
-	WIN32_FIND_DATA names[1000];
+	ull size[N];
+	for (i = 0; i < N; i++) {
+		size[i] = 0;
+	}
+
+	WIN32_FIND_DATA names[N];
 	WIN32_FIND_DATA fdata;
 	HANDLE hfile;
 	wchar_t* path = (wchar_t*)malloc(MAX_PATH * sizeof(wchar_t));
+	wchar_t* wnames = (wchar_t*)malloc(N * sizeof(wchar_t*));
 
 
 
@@ -51,46 +60,93 @@ int main() {
 
 	printf("Your path : %s \n", a);
 	printf("Data in your directory: \n");
+	i = 0;
 
 	do {
 		if (i == 0 || i == 1) {
 			i++;
 			continue;
 		}
-
-
-
 		names[i] = fdata;
-
+		//File name
 		printf("Name:    ");
 		wprintf(names[i].cFileName);
+		wnames[i] = names[i].cFileName;
 		//File size
 		printf(" %10c    Size (bites): %u        ", ' ', names[i].nFileSizeLow);
-		//File creation time
-		time_t *time1 = filetime_to_timet(names[i].ftCreationTime);
-		printf("%10c Created time:  %s     \n", ' ', ctime(&time1));
+		size[i] = names[i].nFileSizeLow;
 		i++;
-	} while (FindNextFileW(hfile, &fdata) != NULL);
+	} while (FindNextFile(hfile, &fdata) != NULL);
 
 	//printf("\n !!!  I hope you knows that a folders is a links, they have no size. You need to input right path if u want to know size of folders.  !!!\n");
 
 
 	char sr;
-	printf("Input a sorted method: \n Input 1 for  insert sort \n Input 2 for bubble sort \n Input 3 for merge sort \n ");
+	printf("Choose a sorting method:\n1 - Bubble\n2 - Inserts\n3 - Merge\n");
 	do {
 		scanf("%c", &sr);
-	} while ((sr != '1') && (sr != '2') && (sr != '3'));	
+	} while ((sr != '1') && (sr != '2') && (sr != '3'));
 
 
 	switch (sr) {
+	case 1: {
+		clock_t begin = clock();
+		bubble_sort(size, ind);
+		clock_t end = clock();
+	}
+	case 2:;
 
-		case 1:
+	case 3:;
+	}
 		
-		case 2:
-		
-		case 3:
+	printf("Sort by:\n1 - ascending\n2- descending");
 
+	char hw;
+	do {
+		scanf("%c", &hw);
+	} while ((hw != '1') && (hw != '2'));
+
+	res(names, size, ind, i, hw);
 	FindClose(hfile);
 	printf("\n END \n");
 	return 0;
 }
+
+	void bubble_sort(int a[], int n[]) {
+		int i, j, tmp;
+		for (i = 0; i < N; i++) {
+			for (j = 0; j < N - i - 1; j++) {
+				if (a[n[j + 1]] < a[n[j]]) {
+					tmp = n[j + 1];
+					n[j + 1] = n[j];
+					n[j] = tmp;
+				}
+			}
+		}
+		
+	}
+
+	void res(WIN32_FIND_DATA names[], ull* size, int *ind, int n, char hw) {
+		switch (hw) {
+		case '1': {
+			for (int i = 2; i < n; i++) {
+				printf("%d.   ", i - 1);
+				printf("Name:    ");
+				int j = ind[i - 2];
+				wprintf(names[j].cFileName);
+				printf(" %10c    Size (bites): %u        ", ' ', names[j].nFileSizeLow);
+				size[j] = names[j].nFileSizeLow;
+			}
+		}
+		case '2': {
+			for (int i = n - 1; i >= 0; i++) {
+				printf("%d.   ", n - i + 2);
+				printf("Name:    ");
+				int j = ind[i];
+				wprintf(names[j].cFileName);
+				printf(" %10c    Size (bites): %u        ", ' ', names[j].nFileSizeLow);
+				size[j] = names[j].nFileSizeLow;
+			}
+		}
+		}
+	}
